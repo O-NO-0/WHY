f369c65fad5bd301c14a7533441893f18c12052e
O-NO-0 <rooshmapraseej@gmail.com>
2020-01-24 17:29:16 +0300

    god y

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var plank;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,7 +37,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    plank =new Log(120,120,100,PI/2);
+    chain = new Chain (bird.body,plank.body);
 }
 
 function draw(){
@@ -63,4 +66,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    plank.display();
+    chain.display();
 }
